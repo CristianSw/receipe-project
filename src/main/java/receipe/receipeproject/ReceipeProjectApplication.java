cf1aa8d7f00a15c512a1387a42d5a0e48ccacd46
CristianSw <cristian.sw@protonmail.com>
2020-03-31 23:10:36 +0300

    Intit commit

---
+++ src/main/java/receipe/receipeproject/ReceipeProjectApplication.java
@@ -0,0 +1,13 @@
+package receipe.receipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/receipe/receipeproject/ReceipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package receipe.receipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
