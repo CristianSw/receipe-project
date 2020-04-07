543f8b72d41abdab584fd9365963fd2979b1b806
CristianSw <cristian.sw@protonmail.com>
2020-04-07 20:38:09 +0300

    added Recipes Ingredient and Notes entity

---
+++ src/main/java/receipe/receipeproject/domain/Ingredient.java
@@ -0,0 +1,48 @@
+package receipe.receipeproject.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+//    private unitOfMeasure uom;
+   @ManyToOne
+    private Recipes recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipes getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Recipes recipes) {
+        this.recipes = recipes;
+    }
+}
